7e492bea4404e0fcf3aae6930190a088d35370fd
Jimmi-Phan <jimmi_phan@hotmail.com>
2025-04-16 13:21:38 +0200

    added line between first two circles

--- eksamensprojekt.py
+++ eksamensprojekt.py
@@ -7,10 +7,10 @@ screen = pg.display.set_mode((0, 0), pg.FULLSCREEN)
 
 active_circles = None
 circles = []
-for i in range(1):
-    x = 50
-    y = 50
-    r = 50
+for i in range(3):
+    x = 50+i * 100
+    y = 50+i * 100
+    r = 25
     circles.append((x, y, r))
 
 ## Game Loop ##
@@ -28,8 +28,8 @@ while running:
         if event.type == pg.MOUSEBUTTONDOWN:
             if event.button == 1:
                 for num, circle in enumerate(circles):
-                    x, y, radius = circle
-                    if (event.pos[0] - x) ** 2 + (event.pos[1] - y) ** 2 <= radius ** 2:
+                    x, y, r = circle
+                    if (event.pos[0] - x) ** 2 + (event.pos[1] - y) ** 2 <= r ** 2:
                         active_circles = num
 
 
@@ -39,15 +39,24 @@ while running:
 
         if event.type == pg.MOUSEMOTION:
             if active_circles != None:
-                x, y, radius = circles[active_circles]
-                circles[active_circles] = (x + event.rel[0], y + event.rel[1], radius)
+                x, y, r = circles[active_circles]
+                circles[active_circles] = (x + event.rel[0], y + event.rel[1], r)
                         
 
 
 
     # Drawing
     screen.fill((128, 128, 128))
+
+    # Draw circles
     for circle in circles:
-        x, y, radius = circle
-        pg.draw.circle(screen, (255, 0, 0), (x, y), radius)
+        x, y, r = circle
+        pg.draw.circle(screen, (50, 50, 50), (x, y), r)
+
+    # Draw a line between the two circles
+    if len(circles) >= 2:
+        x1, y1, _ = circles[0]  # Center of the first circle
+        x2, y2, _ = circles[1]  # Center of the second circle
+        pg.draw.line(screen, (100, 100, 100), (x1, y1), (x2, y2), 2)  # Line with thickness 2
+
     pg.display.flip()
